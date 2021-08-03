8206e2833c79a9eac02d7d3f6ff31c8de1b5876c
EricBalingit <EricBalingit@users.noreply.github.com>
2021-08-03 05:47:32 -0400

    adding file import functionality

--- editor/test/js/SpriteSet.js
+++ editor/test/js/SpriteSet.js
@@ -7,12 +7,18 @@ const fsp = require ( 'fs/promises' );
   * extension - .sps
   * signature - "SPRENGSET"
   * layout type - <byte> 0 (defualt) positioned, 1 fixed
-  * tile count - <int32>
   * position count - <int32>
-  * tile data - <uint8>
+  * packed position size - <int32>
+  * tile count - <int32>
+  * packed tile size <int32>
+  * palette length - <uint8>
+  * packed palette size - <int32>
+  * packed position data - <unit8>
+  * packed tile data - <uint8>
+  * packed palette data - <uint8>
   */
 
-class SpriteSet {
+class SpriteSet extends EventTarget {
     static POSITIONED = 0;
     static FIXED = 1;
     static TYPE = {
@@ -25,7 +31,7 @@ class SpriteSet {
             position = 0,
             signature =  String.fromCharCodes ( ...uint8.slice ( 0, 9 ) );
         if ( singature === 'SPRENGSET' ) {
-            position += 9;
+            position += signature.length;
             let view = new DataView ( data.buffer ),
                 spriteSet = new SpriteSet (),
                 layout = view.getUint8 ( position );
@@ -34,17 +40,26 @@ class SpriteSet {
             } else {
                 throw new TypeError ( 'Unsupported layout type: ' + layout );
             }
-
         } else {
             throw new TypeError ( 'Invalid file signature: ' + signature );
         }
     }
 
     constructor ( src ) {
+        super ();
+        
         if ( typeof src === 'string' ) {
             fsp.readFile ( src ).then ( data => {
-                return SpriteSet.fromBuffer ( data.buffer );
+                Object.assign ( this, SpriteSet.fromBuffer ( data.buffer ) );
+                this.dispatchEvent ( new Event ( 'load' ) )
             } );
         }
+
+        this.type = '';
+        this.width = 0;
+        this.height = 0;
+        this.positions = [];
+        this.tiles = [];
+        this.palette = [];
     }
 }
